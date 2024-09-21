b521400f2dbf1125890675e7c6262dac67e3ab37
warren <thwjd2717@gmail.com>
2024-09-21 19:20:50 +0900

    study: 날짜 포맷 정리

---
+++ basic_type/dates.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: dates parse_string")
+		return
+	}
+	dateString := os.Args[1]
+
+	// 날짜만 존재하는 경우
+	d, err := time.Parse("02 January 2006", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Time:", d.Day(), d.Month(), d.Year())
+	}
+
+	// 날짜 + 시간
+	d, err = time.Parse("02 January 2006 15:04", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Date:", d.Day(), d.Month(), d.Year())
+		fmt.Println("Time:", d.Hour(), d.Minute())
+	}
+
+	// 숫자 형식으로 날짜와 시간 표현된 경우
+	d, err = time.Parse("02-01-2006 15:04", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Date:", d.Day(), d.Month(), d.Year())
+		fmt.Println("Time:", d.Hour(), d.Minute())
+	}
+
+	// 시간만 있는 포맷인 경우
+	d, err = time.Parse("15:04", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Time:", d.Hour(), d.Minute())
+	}
+
+	t := time.Now().Unix()
+	fmt.Println("Epoch time:", t)
+	// 유닉스 시간을 time.Time으로 변환
+	d = time.Unix(t, 0)
+	fmt.Println("Date:", d.Day(), d.Month(), d.Year())
+	fmt.Printf("Time: %d:%d\n", d.Hour(), d.Minute())
+
+	duration := time.Since(start)
+	fmt.Println("Execution time:", duration)
+}
